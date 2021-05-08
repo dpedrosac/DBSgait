6778b2fda48361c087c904cc213d63536984aea5
david <46906261+dpedrosac@users.noreply.github.com>
2021-05-09 00:11:16 +0200

    Separated amplitude, frequency and pulse width

--- analyses.R
+++ analyses.R
@@ -17,7 +17,6 @@ data_dir 	<-	"/media/storage/projekte/DBS_gait/data/"
 df 			<- 	read_excel(paste(data_dir, "gait_parameters_mean.xlsx", sep=""), sheet = "gait_parameters_mean") # load data as dataframe
 df 			<- 	df %>% drop_na("Mean value") # remove NA 
 df 			<- 	as.data.frame(df)
-all_params<- unique(as.factor(df$gait.parameter))
 
 # rename values witin dataframe and convert to "wide format"
 names(df)[names(df) == "Mean value"] 		<- "average.value"
@@ -25,25 +24,42 @@ names(df)[names(df) == "Patient ID"] 			<- "subj"
 names(df)[names(df) == "Test"] 					<- "metric"
 names(df)[names(df) == "Configuration"] 		<- "condition"
 names(df)[names(df) == "Gait Parameter"] 	<- "gait.parameter"
+all_params<- unique(as.factor(df$gait.parameter))
 
-df_eyeball 						<- df[which(df$gait.parameter=="Mean Gait Speed [m/s]" & df$metric=="free"),] 
+df_eyeball 						<- df[which(df$gait.parameter=="Cv Stride Length [cm]" & df$metric=="fast"),] 
 df_eyeball 						<- na.omit(df_eyeball)
 df_eyeball$subj 					<- as.factor(df_eyeball$subj)
 df_eyeball$condition 			<- as.factor(df_eyeball$condition)
 df_eyeball$average.value	<- as.numeric(df_eyeball$average.value)
 
-# Normalisation
-idx_subj 				<- df_eyeball$subj[df_eyeball$condition == "OFF"] # data normalised to "OFF" condition so analyses restricted too those who were recorded in the "OFF"
-df_eyeball 			<- df_eyeball[is.element(df_eyeball$subj, idx_subj),]
-df_eyeball 			<- df_eyeball %>%
+# Separate data and normalise to "OFF" condition
+idx_subj 				<- df_eyeball$subj[df_eyeball$condition == "OFF"] # data normalised to "OFF" condition so analyses restricted to those who were recorded in the "OFF"
+groups = list(as.factor(c("OFF", "033", "066", "100")), as.factor(c("OFF", "040", "090", "130")), as.factor(c("OFF", "030", "085", "130")))
+names  = c("amplitude", "pulse_width", "frequency")
+for(i in 1:length(groups)) {
+ df_temp <- df_eyeball[is.element(df_eyeball$subj, idx_subj),]
+ df_temp <- df_temp[is.element(df_temp$condition, groups[[i]]),]
+
+ # Normalisation
+df_temp 			<- df_temp[is.element(df_temp$subj, idx_subj),]
+df_temp 			<- df_temp %>%
     group_by(subj) %>%
     mutate(normalised.value = average.value / average.value[condition == "OFF"]) # Normalisation
+ assign(names[i], df_temp) 
+ }
 
-p1 <- ggplot(df_eyeball, aes(x=condition, y=normalised.value, group=subj, color=subj)) +
+p1 <- ggplot(amplitude, aes(x=condition, y=normalised.value, group=subj, color=subj)) +
+geom_point(stat='summary', fun=sum) +
+stat_summary(fun=sum, geom="line") + 
+theme_minimal()
+
+p2 <- ggplot(pulse_width, aes(x=condition, y=normalised.value, group=subj, color=subj)) +
 geom_point(stat='summary', fun=sum) +
 stat_summary(fun=sum, geom="line")
 
-# Problems right now: Dimensionaly reduction seem reasonable (PCA, k-nearest neighbours, etc.), a different approach might be more 
-# intuitive to understand, since 030 and 033 are not similat but different settings (amplitude vs. different frequency at high amplitude respectively). 
-# For the latter separate plots for frequency (30, 90) vs. OFF, amplitude (033, 066, 100) vs. OFF and pulse width (040, 085) vs. OFF. 
-# Small question: what is the difference between 130Hz and 100?! pulse width?
+p3 <- ggplot(frequency, aes(x=condition, y=normalised.value, group=subj, color=subj)) +
+geom_point(stat='summary', fun=sum) +
+stat_summary(fun=sum, geom="line")
+
+# Problems right now: Dimensionaly reduction seem reasonable (PCA, k-nearest neighbours, etc.); Why aren't we looking at the two sensors separately?
+# with so many data, differentiating the two sides would be a small effort and could give more insight.
