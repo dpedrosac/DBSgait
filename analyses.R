878809b65fb319de40b54eddcdb09f31d5d9a359
david <46906261+dpedrosac@users.noreply.github.com>
2021-05-09 22:58:24 +0200

    Update on correlation matrices

--- analyses.R
+++ analyses.R
@@ -114,3 +114,18 @@ p4 <- ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
     size = 12, hjust = 1))+
  coord_fixed()
+
+p4 +
+geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
+theme(
+  axis.title.x = element_blank(),
+  axis.title.y = element_blank(),
+  panel.grid.major = element_blank(),
+  panel.border = element_blank(),
+  panel.background = element_blank(),
+  axis.ticks = element_blank(),
+  legend.justification = c(1, 0),
+  legend.position = c(0.6, 0.7),
+  legend.direction = "horizontal")+
+  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                title.position = "top", title.hjust = 0.5))
