05a0ae145cef24859bdd1c505dca9bff7f1aa983
Kleinholdermann <urs@kleinholdermann.de>
2021-11-05 19:50:21 +0100

    update of local_functions and data

--- analysis/local-functions.R
+++ analysis/local-functions.R
@@ -21,26 +21,63 @@ get_dvnames <- function(df){
 return(names(df)[grep("_", names(df))])
 }
 
-get_data <- function(off.normalized = FALSE, type = "aggregated"){
+outlier_to_na <- function(x, nsigma = 3){
+   mu    = mean(x)
+   sigma = sd(x)
+   x[x < mu - nsigma * sigma | x > mu + nsigma * sigma] = NA
+return(x)   
+}
+
+get_data <- function( off_normalized = FALSE
+                    , type = "aggregated"
+                    , remove_outliers = FALSE){
 # get local constants and paths
 source("local-constants.R")
 lc = local_constants()
 
 if(type == "aggregated") datafile = "gait_params.csv"
 if(type == "individual") datafile = "stride_params.csv"    
+if(type == "clinical"){
+  df = read.csv(file.path(lc$paths$data,"clinical_data.csv")
+               , stringsAsFactors = FALSE, sep = ";")
+  # make coding of pat-ID the same as in the other data:
+  names(df)[names(df) == 'Patient'] = 'id'
+  ix = grep("P[1-9]$", df$id)
+  df$id[ ix] = gsub("P","Pat_0",df$id[ ix])
+  df$id[-ix] = gsub("P","Pat_" ,df$id[-ix])
+  return(df)
+}
     
 # read in data
 df = read.csv(file.path(lc$paths$data, datafile)
-             , header = TRUE, stringsAsFactors = FALSE
+             , header = TRUE
+             , stringsAsFactors = FALSE
+             , sep = ";"
              , na.strings = "NaN")
 df[is.na(df)] = NA # hack for different types of NA in df
 
-# normalize data by subtracting off condition mean
-if(off.normalized){
-  varnames = setdiff(names(df), c("patient_id", "test", "configuration"))
-  df = group_reference(varnames, df)
+# do some renaming for convenient filtering of dependent variables by undescores
+names(df)[names(df) == 'patient_id'] = 'id'    
+if(type == "individual"){
+  names(df)[names(df) == 'stride_id'] = 'stride'
+  names(df)[names(df) == 'time_stamp_s'] = 'timestamp'    
 }
-
+    
+# remove outliers if requested
+if(remove_outliers){
+  # finding cols by using _ includes also the percent values. it might not
+  # be so sensible to cut at x sigmas here...
+  numeric_cols =  names(df)[grep("_", names(df))] 
+  df[numeric_cols] = lapply(df[numeric_cols], outlier_to_na) 
+}
+    
+# normalize data by subtracting off condition mean
+if(off_normalized){
+  numeric_cols =  names(df)[grep("_", names(df))] 
+#  varnames = setdiff(names(df), c("patient_id", "test", "configuration"))
+  df = group_reference(numeric_cols, df)
+}
+    
 # define the dbs conditions for convenient filtering
 df$dbscond = NA
 df$dbscond[df$configuration == "040" | df$configuration == "090"] = "pulse"
@@ -51,13 +88,6 @@ df$dbscond[  df$configuration == "033"
            | df$configuration == "066"
            | df$configuration == "100"] = "strength"
 df$dbscond[df$configuration == "OFF"] = "OFF"
-
-# do some renaming for convenient filtering of dependent variables by undescores
-names(df)[names(df) == 'patient_id'] = 'id'    
-if(type == "individual"){
-  names(df)[names(df) == 'stride_id'] = 'stride'
-  names(df)[names(df) == 'time_stamp_s'] = 'timestamp'    
-}
     
 return(df)
 } # end of get_data function                
@@ -65,9 +95,9 @@ return(df)
 group_reference <- function(varnames, df){
 # subtract OFF condition mean from data
 for(testname in unique(df$test)){
-  for(pat in unique(df$patient_id)){
+  for(pat in unique(df$id)){
     for(var in varnames){
-      ix = df$test == testname & df$patient_id == pat
+      ix = df$test == testname & df$id == pat
       mu = mean(df[[var]][ix & df$configuration == "OFF"], na.rm = TRUE)
       df[[var]][ix] = df[[var]][ix] - mu
     } # end of var loop
